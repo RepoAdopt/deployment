d73d55becf79efd3c105064aba663d82d29f0b1e
BeauTaapken <beau@lioncode.nl>
2021-05-14 10:55:11 +0200

    Add files via upload

---
+++ demo deployment/demo_windows.bat
@@ -0,0 +1,13 @@
+git init
+git clone https://github.com/RepoAdopt/deployment
+
+@echo Pulled repoadopt deployment
+
+cd deployment
+
+docker-compose up
+
+@echo
+@echo repoadopt has started. Go to localhost:8080 to access
+
+pause
